da160508586a710a17b168e9b0e9137dbfd95344
akashjha1997 <akashjha1997.aj@gmail.com>
2021-02-24 15:08:09 +0530

    Update Problem3.java

--- Assign/Assignment/Problem/Problem3.java
+++ Assign/Assignment/Problem/Problem3.java
@@ -1,23 +1,29 @@
 package com.Assignment.Problem;
 
-import java.util.Set;
-import java.util.TreeSet;
-import java.util.regex.Pattern;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
 
 public class Problem3 {
-public static void main(String[] args) {
-	//TreeSet for ascending order 
-	Set<Integer> set=new TreeSet<>();
-	String str="asdsdgbdfgjdfgn6sbdfh3hhj5j1jh5j66asryuop8ng2nb7mbb8k;0++)O)";
-	for(int i=0;i<str.length();i++) {
-			if(Character.isDigit(str.charAt(i))) {
-				int num=Character.getNumericValue(str.charAt(i));
-				set.add(num);
+	public static void main(String[] args) {
+		// TreeSet for ascending order
+		List<Integer> list = new ArrayList<>();
+		String str = "asdsdgbdfgjdfgn6sbdfh3hhj5j1jh5j66asryuop8ng2nb7mbb8k;0++)O)";
+		for (int i = 0; i < str.length(); i++) {
+			if (Character.isDigit(str.charAt(i))) {
+				int num = Character.getNumericValue(str.charAt(i));
+				list.add(num);
 			}
-			
-		
+
+		}
+		//sorting the list
+		Collections.sort(list);
+		System.out.println(list);
+		String newString = list.toString();
+		//manipulating the string 
+		String newString1 = newString.substring(1, newString.length() - 1).replace(", ", "");
+		Long l = Long.parseLong(newString1);
+		System.out.println(l);
 	}
-	
-	System.out.println(set.iterator().next());
-}
 }
